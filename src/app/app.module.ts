2e42ce61cbfd562800688d9853999f83b67a8d81
Dimorg <mathiasperis@gmail.com>
2023-05-17 13:10:23 +0200

    Finition HEader + debut section profil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgParticlesModule } from 'ng-particles';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PresentationComponent } from './components/presentation/presentation.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, PresentationComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NgParticlesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -8,5 +8,47 @@ import { Component, OnInit } from '@angular/core';
 export class PresentationComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    //Animation machine a ecrire
+    let isTyping = false;
+    const paragraphe = document.querySelector('.p1') as HTMLElement;
+    const text =
+      'Je suis Mathias, en plein reconversion en tant que développeur web, voici mon modeste portfollio.';
+
+    //Fonction machine a ecrire au tableau
+    function typeWriter(word: string, index: number, onComplete: () => void) {
+      if (index < word.length) {
+        setTimeout(() => {
+          const span = document.createElement('span');
+          span.textContent = word[index];
+          span.style.fontFamily = 'Fredericka the Great, cursive';
+          paragraphe.appendChild(span);
+          typeWriter(word, index + 1, onComplete);
+        }, 100);
+      } else {
+        onComplete();
+      }
+    }
+    //Fonction qui declenche l'ecriture au scroll
+    function typeWriterOnScroll() {
+      const { scrollTop, clientHeight } = document.documentElement;
+      const topElementToTopViewport = paragraphe.getBoundingClientRect().top;
+
+      if (
+        scrollTop >
+        parseFloat((scrollTop + topElementToTopViewport).toFixed()) -
+          Number(clientHeight) * 0.5
+      ) {
+        if (!isTyping) {
+          isTyping = true;
+          typeWriter(text, 0, () => {
+            window.removeEventListener('scroll', typeWriterOnScroll);
+            isTyping = false;
+          });
+        }
+      }
+    }
+    //Envoie de la fonction typeWritter
+    window.addEventListener('scroll', typeWriterOnScroll);
+  }
 }
